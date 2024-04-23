dddec9392dd6c8880d35282662db76ef283903e2
Timourka <timourkot@gmail.com>
2024-04-24 00:30:01 +0400

    5лаба

--- git_commit.bat
+++ git_commit.bat
@@ -5,6 +5,9 @@ setlocal
 :: Запрашиваем название коммита
 set /p commit_message="Введите название коммита: "
 
+:: Запрашиваем имя ветки с помощью значения по умолчанию 'main'
+set /p branch_name="Введите имя ветки (по умолчанию 'main'): " || set branch_name=main
+
 :: Проверяем, находится ли скрипт в репозитории Git
 git rev-parse --is-inside-work-tree > nul 2>&1
 if %errorlevel% equ 0 (
@@ -14,10 +17,10 @@ if %errorlevel% equ 0 (
     :: Делаем коммит с заданным сообщением
     git commit -m "%commit_message%"
     
-    :: Отправляем коммит в удаленное хранилище
-    git push origin master
+    :: Отправляем коммит в удаленное хранилище на указанную ветку
+    git push origin %branch_name%
     
-    echo Коммит с названием "%commit_message%" успешно отправлен в удаленное хранилище.
+    echo Коммит с названием "%commit_message%" успешно отправлен в удаленное хранилище на ветку "%branch_name%".
 ) else (
     echo Ошибка: скрипт не находится в репозитории Git.
 )
